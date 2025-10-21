ee58b4cacf0dd07e0b665bd32b21d83500954ed7
evgenii64000 <evgenii64000@yandex.ru>
2025-10-21 18:43:47 +0500

    test

--- src/CalculationException.java
+++ src/CalculationException.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
+public class CalculationException extends NumberFormatException {
 
-public class Exception {
+    public CalculationException (String description) {
+        super(description);
+    }
+
 }
--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+class Calculator {
 
-public class Calculator {
+    int calculate (String line) throws CalculationException {
+        String[] mathSigh = line.split(" ");
+        int a = Integer.parseInt(mathSigh[0]);
+        String operator = mathSigh[1];
+        int b = Integer.parseInt(mathSigh[2]);
+        switch (operator) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b;
+            default: throw new  CalculationException("Неверный математический оператор");
+        }
+    }
+
+    boolean checkInput (String input) throws CalculationException {
+        if (input.isEmpty()) {
+            throw new CalculationException("Введена пустая строка");
+        }
+        String[] mathSigh = input.split(" "); //Добавить проверку на размер числа
+        if (mathSigh.length != 3) {
+            throw new CalculationException("Неверный формат примера");
+        }
+        int a = 0;
+        int b = 0;
+        try {
+            a = Integer.parseInt(mathSigh[0]);
+            b = Integer.parseInt(mathSigh[2]);
+        } catch (NumberFormatException e) {
+            throw new CalculationException("Введено не целое число");
+        }
+        if (a > 10 || a < 1 || b > 10 || b < 1) {
+            throw new CalculationException("Числа выходят за допустимый диапазон");
+        }
+        return true;
+    }
 }
